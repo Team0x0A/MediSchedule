1047eb546b4ded98c11c02f947a0623a899b5495
JustinWang123 <jgw4@sfu.ca>
2013-10-27 19:39:14 -0700

    Adding test cases

--- MediScheduleTests/MediScheduleTests.m
+++ MediScheduleTests/MediScheduleTests.m
@@ -14,15 +14,44 @@
  */
 
 #import "MediScheduleTests.h"
+#import "ReminderManager.h"
+#import "Time.h"
 
 @implementation MediScheduleTests
+
+
 
 - (void)setUp
 {
     [super setUp];
     
-    // Set-up code here.
+    ReminderManager* testManager = [[ReminderManager alloc] init];
+    
+    // Add 10 reminders in order:
+    for(int i = 0; i < 10; i++)
+    {
+        [testManager addReminderWithTime:[[Time alloc] initWithHour:i WithMin:i WithSec:i] WithPillId:i WithDosage:i WithNotes:@"Some Notes"];
+    }
+    
+    // Test that the values were set correctly:
+    for(int i = 0; i < 10; i++)
+    {
+        if( [[testManager timeAt:i] hour] != i
+        ||  [[testManager timeAt:i] min] != i
+        ||  [[testManager timeAt:i] sec] != i)
+        {
+            STFail(@"Reminder Manager Failed...");
+        }
+    }
+         
+
+    [testManager deleteReminderWith:0];
+    [testManager deleteReminderWith:0];
+    [testManager deleteReminderWith:0];
+    [testManager deleteReminderWith:0];
 }
+
+
 
 - (void)tearDown
 {
@@ -30,6 +59,8 @@
     
     [super tearDown];
 }
+
+
 
 - (void)testExample
 {
