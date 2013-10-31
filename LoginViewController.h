be5e93327ab02710179913de693824398d09f237
Justin <jgw4@sfu.ca>
2013-10-30 18:02:24 -0700

    6:02 Final Commit

---
+++ LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  MediSchedule
+//
+//  Created by Justin Wang on 10/30/13.
+//  Copyright (c) 2013 Ishan Bhutani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+//***************************************************************************************
+// Public Interface:
+//***************************************************************************************
+@interface LoginViewController : UIViewController <UITextFieldDelegate>
+
+@end
